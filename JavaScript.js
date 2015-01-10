e8a3984bfe7cd04e06bfd6212a52fca25ec00018
kevink1103 <kevink1103@gmail.com>
2015-01-11 03:05:24 +0900

    Reserved Money
    Added calculating the amount of reserved money

--- JavaScript.js
+++ JavaScript.js
@@ -4,12 +4,22 @@ var max_result = $('.max_result').val();
 
 var main = function() {
   $('.principal').keyup(function() {
+    var principal = $('.principal').val();
+    var reserved = $('.reserved').val();
+    var max_result = $('.max_result').val();
+
     reserved = principal / 10;
-    alert(principal);
+    $('.reserved').text(reserved);
   });
 
   $('.btn').click(function() {
     var principal = $('.principal').val();
+    var reserved = $('.reserved').val();
+    var max_result = $('.max_result').val();
     alert(principal);
+    alert(reserved);
+    alert(max_result);
   });
-}
+};
+
+$(document).ready(main);
