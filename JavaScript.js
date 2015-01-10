8a85b3dfed701e3e48895cf41c2ff5b75535da93
kevink1103 <kevink1103@gmail.com>
2015-01-11 02:36:13 +0900

    Shadow Extended
    Shadow extened to 150

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,15 @@
 var principal = $('.principal').val();
+var reserved = $('.reserved').val();
 var max_result = $('.max_result').val();
 
 var main = function() {
   $('.principal').keyup(function() {
+    reserved = principal / 10;
+    alert(principal);
+  });
 
+  $('.btn').click(function() {
+    var principal = $('.principal').val();
+    alert(principal);
   });
 }
